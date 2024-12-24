9176b5abc5508d23d949c7b0f4c93a926a67bacb
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-12-25 08:59:48 +0900

    chore : 린트 적용

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,31 +1,31 @@
-import mariadb from "mariadb";
-import dotenv from "dotenv";
+import mariadb from 'mariadb'
+import dotenv from 'dotenv'
 
 // 환경 변수 로드
-dotenv.config();
+dotenv.config()
 
 // MariaDB 연결 풀 생성
 const pool = mariadb.createPool({
-  host: process.env.MDB_HOST, // MariaDB 서버 주소 (로컬일 경우 localhost)
-  user: process.env.MDB_USER, // 데이터베이스 사용자
-  password: process.env.MDB_PASS, // 비밀번호
-  database: process.env.MDB_DATABASE, // 사용할 데이터베이스
-  port: process.env.MDB_PORT, // MariaDB 포트 (기본값: 3306)
-});
+	host: process.env.MDB_HOST, // MariaDB 서버 주소 (로컬일 경우 localhost)
+	user: process.env.MDB_USER, // 데이터베이스 사용자
+	password: process.env.MDB_PASS, // 비밀번호
+	database: process.env.MDB_DATABASE, // 사용할 데이터베이스
+	port: process.env.MDB_PORT, // MariaDB 포트 (기본값: 3306)
+})
 
 // 연결 풀 반환
 async function getConnection() {
-  try {
-    const conn = await pool.getConnection();
-    return conn;
-  } catch (err) {
-    console.error("데이터베이스 연결 오류:", err);
-    throw err;
-  }
+	try {
+		const conn = await pool.getConnection()
+		return conn
+	} catch (err) {
+		console.error('데이터베이스 연결 오류:', err)
+		throw err
+	}
 }
 
 // `pool`을 default로 내보내기
-export default pool;
+export default pool
 
 // 필요한 경우 `getConnection`도 내보내기
-export { getConnection };
+export { getConnection }
--- src/routes/test_boongRoutes.js
+++ src/routes/test_boongRoutes.js
@@ -1,13 +1,12 @@
-import { Router } from 'express';
-import { getBoongList } from '../controllers/test_boongController.js';
+import { Router } from 'express'
+import { getBoongList } from '../controllers/test_boongController.js'
 import { registerUser } from '../controllers/test_boongController.js'
-const router = Router();
+const router = Router()
 
 // GET /boongs - 붕어빵 리스트 가져오기
-router.get('/boongs', getBoongList);
+router.get('/boongs', getBoongList)
 
 // POST 요청을 /register로 보내면 사용자 정보 저장
-router.post('/register', registerUser);
+router.post('/register', registerUser)
 
-export default router;
-
+export default router
