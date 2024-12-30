a8ebb1e692b5170a9ecd1749c82c18be3ff26efb
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-12-30 16:38:14 +0900

    FEAT : 카카오 로그인 - 카카오 서버연결

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,43 +1,95 @@
 // import { saveUserToDB } from '../models/LoginModel'
 
-export async function requestKakaoLogin(params) {
-    console.log('requestKakaoLogin')
+// todo 임시로 키값 다이렉트 입력
+const KAKAO_RESET_API = 'f65039729f458b8346277ff5a9799a1a'
+const REDIRECT_URI = 'http://192.168.162.10:8081/'
 
-    return 
+export async function requestKakaoLogin(req, res) {
+	if (!req.body.code) {
+		return res.status(400).json({
+			code: 400,
+			message: 'authorizaion code가 없습니다.',
+		})
+	}
+
+	try {
+		const tokenData = await getKakaoToken(req, res)
+        const myInfo = await getKakaoInfo(tokenData.access_token, req, res)
+
+		return res.status(201).json({
+			code: 201,
+			msg: 'Login successful',
+			data: {
+				user_id: myInfo.id,
+				nickname: myInfo.kakao_account.profile.nickname,
+				token: tokenData.access_token,
+			},
+		})
+	} catch (error) {
+		return res.status(401).json({
+			code: 401,
+			msg: 'Invalid access token',
+		})
+	}
 }
 
-// const saveUser = async (req, res) => {
-// 	const { id, nickname } = req.body
+async function getKakaoToken(req, res) {
+	const url = 'https://kauth.kakao.com/oauth/token'
+	const params = new URLSearchParams({
+		grant_type: 'authorization_code',
+		client_id: KAKAO_RESET_API,
+		redirect_uri: REDIRECT_URI,
+		code: req.body.code,
+	})
 
-// 	if (!id || !nickname) {
-// 		return res
-// 			.status(400)
-// 			.json({ message: '사용자 ID와 닉네임이 필요합니다.' })
-// 	}
+	try {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type':
+					'application/x-www-form-urlencoded;charset=utf-8',
+			},
+			body: params.toString(),
+		})
 
-// 	try {
-// 		const result = await saveUserToDB(id, nickname) // 모델의 saveUser 호출
+		const token = await response.json()
+		if (token.error == 'invalid_grant') {
+			console.log('invalid_grant')
+			return res.status(401).json({
+				code: 401,
+				msg: 'Invalid access token',
+			})
+		}
+		return token
+	} catch (error) {
+		console.error('Error in getKakaoToken:', error)
+		return res.status(401).json({
+			code: 400,
+			msg: 'no access token',
+		})
+	}
+}
 
-// 		// 쿼리 실행 결과 확인
-// 		console.log('DB 저장 결과:', result) // 결과 출력
-
-// 		if (result.affectedRows > 0) {
-// 			res.status(200).json({
-// 				message: '사용자 데이터 저장 성공!',
-// 				user: { id, nickname },
-// 			})
-// 		} else {
-// 			res.status(400).json({
-// 				message: '사용자 데이터 저장 실패 (변경된 데이터 없음)',
-// 			})
-// 		}
-// 	} catch (error) {
-// 		console.error('DB 저장 중 오류:', error)
-// 		res.status(500).json({
-// 			message: '서버 오류로 인해 데이터 저장에 실패했습니다.',
-// 			error: error.message,
-// 		})
-// 	}
-// }
-
-// export { saveUser }
+async function getKakaoInfo(ACCESS_TOKEN, req, res) {
+    const url = 'https://kapi.kakao.com/v2/user/me'
+    const params = new URLSearchParams({
+        secure_resource: 'false',
+        property_keys: JSON.stringify(['kakao_account.profile']),
+    });
+    try {
+		const response = await fetch(`${url}?${params.toString()}`, {
+			method: 'GET',
+			headers: {
+                'Authorization' : `Bearer ${ACCESS_TOKEN}`,
+				'Content-Type':'application/x-www-form-urlencoded;charset=utf-8',
+			},
+		})
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return res.status(401).json({
+			code: 500,
+			msg: 'kakao server error',
+		})
+    }
+}
--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import * as authRouter from '../controllers/AuthController.js'
+import * as AuthController from '../controllers/AuthController.js'
 
 // import { Router } from 'express'
 
 const route = express.Router()
 
 // 클라이언트가 서버에 카카오 로그인 요청
-route.post('/', authRouter.requestKakaoLogin)
+route.post('/', AuthController.requestKakaoLogin)
 
 export default route
