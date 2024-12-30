39b56d36e0d48e2f23863600a99eca46827fba29
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-12-30 23:35:51 +0900

    FEAT : 카카오 로그인 토큰 저장
    - errorCode 정리 했습니다.
    - 테스트 못해봤습니다.
    - 린트 돌렸습니다.

--- src/connection.js
+++ src/connection.js
@@ -1,16 +1,16 @@
-import mysql from 'mysql2/promise';
-import { DB_CONFIG } from '../config.js';
+import mysql from 'mysql2/promise'
+import { DB_CONFIG } from '../config.js'
 
-let db;
+let db
 
 export const connectDB = async () => {
-    try {
-        db = await mysql.createConnection(DB_CONFIG);
-        console.log('DB 연결 성공');
-    } catch (err) {
-        console.error('DB 연결 실패:', err);
-        throw err;
-    }
-};
+	try {
+		db = await mysql.createConnection(DB_CONFIG)
+		console.log('DB 연결 성공')
+	} catch (err) {
+		console.error('DB 연결 실패:', err)
+		throw err
+	}
+}
 
-export const getDB = () => db;
+export const getDB = () => db
--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,24 +1,30 @@
-// import { saveUserToDB } from '../models/LoginModel'
+import { KAKAO_CONFIG } from '../config'
+import { saveUserToDB } from '../models/loginModel'
+import errorCode from '../util/error'
 
-// todo 임시로 키값 다이렉트 입력
-const KAKAO_RESET_API = 'f65039729f458b8346277ff5a9799a1a'
-const REDIRECT_URI = 'http://192.168.162.10:8081/'
+const KAKAO_RESET_API = KAKAO_CONFIG.REST_KEY
+const REDIRECT_URI = KAKAO_CONFIG.REDIRECT_URI
+const GET_KAKAO_TOKEN_URL = 'https://kauth.kakao.com/oauth/token'
+const GET_KAKAO_INFO_URL = 'https://kapi.kakao.com/v2/user/me'
 
 export async function requestKakaoLogin(req, res) {
 	if (!req.body.code) {
-		return res.status(400).json({
-			code: 400,
-			message: 'authorizaion code가 없습니다.',
-		})
+		return res.status(404).json(errorCode[404])
 	}
 
 	try {
 		const tokenData = await getKakaoToken(req, res)
-        const myInfo = await getKakaoInfo(tokenData.access_token, req, res)
+		const myInfo = await getKakaoInfo(tokenData.access_token, res)
+
+		await saveUserToDB(
+			myInfo.id,
+			myInfo.kakao_account.profile.nickname,
+			tokenData.access_token,
+		).catch((err) => res.status(501).json(errorCode[501]))
 
 		return res.status(201).json({
 			code: 201,
-			msg: 'Login successful',
+			msg: 'Login Successful',
 			data: {
 				user_id: myInfo.id,
 				nickname: myInfo.kakao_account.profile.nickname,
@@ -26,15 +32,12 @@ export async function requestKakaoLogin(req, res) {
 			},
 		})
 	} catch (error) {
-		return res.status(401).json({
-			code: 401,
-			msg: 'Invalid access token',
-		})
+		return res.status(400).json(errorCode[400])
 	}
 }
 
 async function getKakaoToken(req, res) {
-	const url = 'https://kauth.kakao.com/oauth/token'
+	const url = GET_KAKAO_TOKEN_URL
 	const params = new URLSearchParams({
 		grant_type: 'authorization_code',
 		client_id: KAKAO_RESET_API,
@@ -54,42 +57,38 @@ async function getKakaoToken(req, res) {
 
 		const token = await response.json()
 		if (token.error == 'invalid_grant') {
-			console.log('invalid_grant')
-			return res.status(401).json({
-				code: 401,
-				msg: 'Invalid access token',
-			})
+			return res.status(404).json(errorCode[404])
 		}
 		return token
 	} catch (error) {
-		console.error('Error in getKakaoToken:', error)
-		return res.status(401).json({
-			code: 400,
-			msg: 'no access token',
+		return res.status(500).json({
+			...errorCode[500],
+			detail: error,
 		})
 	}
 }
 
-async function getKakaoInfo(ACCESS_TOKEN, req, res) {
-    const url = 'https://kapi.kakao.com/v2/user/me'
-    const params = new URLSearchParams({
-        secure_resource: 'false',
-        property_keys: JSON.stringify(['kakao_account.profile']),
-    });
-    try {
+async function getKakaoInfo(access_token, res) {
+	const url = GET_KAKAO_INFO_URL
+	const params = new URLSearchParams({
+		secure_resource: 'false',
+		property_keys: JSON.stringify(['kakao_account.profile']),
+	})
+	try {
 		const response = await fetch(`${url}?${params.toString()}`, {
 			method: 'GET',
 			headers: {
-                'Authorization' : `Bearer ${ACCESS_TOKEN}`,
-				'Content-Type':'application/x-www-form-urlencoded;charset=utf-8',
+				Authorization: `Bearer ${access_token}`,
+				'Content-Type':
+					'application/x-www-form-urlencoded;charset=utf-8',
 			},
 		})
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        return res.status(401).json({
-			code: 500,
-			msg: 'kakao server error',
+		const data = await response.json()
+		return data
+	} catch (error) {
+		return res.status(500).json({
+			...errorCode[500],
+			detail: error,
 		})
-    }
+	}
 }
