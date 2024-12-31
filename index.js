1b37e3b24fb8394f11f1dfe58b96235fc081377a
LeejongMIn <151716813+leeJongMin58@users.noreply.github.com>
2024-12-31 09:00:03 +0900

    Revert "Revert "develop/BT-79 : BoongTam-server develop  카카오  로그인 및 토큰 발행""

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import app from './src/app.js';
-import { connectDB } from './src/database/connection.js';
+import app from './src/app.js'
+import { connectDB } from './src/database/connection.js'
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 const startServer = async () => {
-  try {
-      await connectDB(); // DB 연결 초기화
-      app.listen(PORT, () => {
-          console.log(`서버가 ${PORT}번 포트에서 매우 잘 실행중`);
-      });
-  } catch (error) {
-      console.error('서버 시작 중 오류:', error.message);
-      process.exit(1);
-  }
-};
+	try {
+		await connectDB() // DB 연결 초기화
+		app.listen(PORT, () => {
+			console.log(`서버가 ${PORT}번 포트에서 매우 잘 실행중`)
+		})
+	} catch (error) {
+		console.error('서버 시작 중 오류:', error.message)
+		process.exit(1)
+	}
+}
 
-startServer();
+startServer()
