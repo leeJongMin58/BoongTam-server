dc10d06f57635f6f6cfd7dbc4af00a1cb0cb3dbf
pokmonlove <kelly94579457@gmail.com>
2025-01-20 21:54:41 +0900

    주석 삭제

--- src/controllers/boongController.js
+++ src/controllers/boongController.js
@@ -7,65 +7,6 @@ import {
 } from '../models/storeModel.js'
 import errorCode from '../util/error.js'
 
-// export const getNearbyStores = async (req, res) => {
-// 	// 요청 바디에서 좌표 값 추출
-// 	const { lat, lng, lat_lu, lng_lu, lat_rd, lng_rd } = req.body;
-// 	const { count, sort } = req.query; // sort(정렬) 추가
-// 	console.log('req.query:', req.query);
-// 	// 파라미터가 모두 있는지 확인하고, 숫자로 변환
-// 	if (
-// 		!lat ||
-// 		!lng ||
-// 		!lat_lu ||
-// 		!lng_lu ||
-// 		!lat_rd ||
-// 		!lng_rd ||
-// 		isNaN(parseFloat(lat)) ||
-// 		isNaN(parseFloat(lng)) ||
-// 		isNaN(parseFloat(lat_lu)) ||
-// 		isNaN(parseFloat(lng_lu)) ||
-// 		isNaN(parseFloat(lat_rd)) ||
-// 		isNaN(parseFloat(lng_rd))
-// 	) {
-// 		return res.status(400).json({
-// 			...errorCode[400],
-// 			detail: '모든 좌표를 올바른 형식으로 입력해주세요.',
-// 		});
-// 	}
-// 	// count가 숫자인지 확인
-// 	const storeCount = count && !isNaN(Number(count)) ? Number(count) : 5; // 기본값 5
-
-// 	// sort가 all 또는 popular인지 확인, 기본값은 'all' (가까운 거리 순)
-// 	const sortOrder = sort === 'popular' ? 'ORDER BY heart_count DESC' : 'ORDER BY distance ASC';
-
-// 	try {
-// 		// fetchNearbyStores 호출 시, 파라미터를 올바르게 전달
-// 		const stores = await fetchNearbyStores(            
-// 			parseFloat(lat),
-// 			parseFloat(lng),
-// 			parseFloat(lat_lu),
-// 			parseFloat(lng_lu),
-// 			parseFloat(lat_rd),
-// 			parseFloat(lng_rd),
-// 			storeCount,
-// 			sortOrder 
-// 		);
-
-// 		// 성공적인 응답 반환
-// 		res.status(200).json({
-// 			code: 200,
-// 			msg: '통신 성공',
-// 			count: stores.length,
-// 			data: stores,
-// 		});
-// 	} catch (error) {
-// 		console.error('컨트롤러 오류:', error);
-// 		res.status(500).json(
-// 			errorCode[500]
-// 		);
-// 	}
-// };
-
 export const getNearbyStores = async (req, res) => {
 	// 요청 바디에서 lat, lng 추출
 	const { lat, lng } = req.body;
