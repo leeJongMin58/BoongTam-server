6cf2656741c2614417ab569d1de5251a77329eaf
pokmonlove <kelly94579457@gmail.com>
2025-01-21 17:28:41 +0900

    커뮤니티, jwt 방식 수정

--- src/routes/communityRoutes.js
+++ src/routes/communityRoutes.js
@@ -7,16 +7,16 @@ import { authenticateToken } from '../middleware/authMiddleware.js';
 const router = Router()
 
 // 매장 제보하기 
-router.post('/store', authenticateToken, createStoreReport)
+router.post('/store', createStoreReport)
 
 // 매장 리뷰 작성
-router.post('/store/review', authenticateToken, createStoreReview)
+router.post('/store/review', createStoreReview)
 
 // 굿즈 리뷰 작성
-router.post('/goods/review', authenticateToken, createGoodsReview)
+router.post('/goods/review', createGoodsReview)
 
 // 하트 토글
-router.patch('/re/like', authenticateToken, ReviewLikeController.updateLikeStatus);
+router.patch('/re/like', ReviewLikeController.updateLikeStatus);
 
 // 매장 리뷰 조회
 router.get('/sre', getStoreReviews);
