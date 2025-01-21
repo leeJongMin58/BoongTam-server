97d82986c7185760d5dd0274fc68829722a161bd
pokmonlove <kelly94579457@gmail.com>
2025-01-21 11:35:03 +0900

    map api_썸네일, 리뷰 개수 추가

--- src/services/boongService.js
+++ src/services/boongService.js
@@ -3,22 +3,27 @@ import { getDB } from '../database/connection.js'
 export const fetchNearbyStores = async (lat, lng, radius = 5, count = 5, sortOrder = 'ORDER BY distance ASC') => {
 	try {
 		// 반경 필터링을 위한 쿼리 작성
+		// 매장 및 리뷰 개수를 가져오는 쿼리
 		const query = `
             SELECT 
-                store_id, 
-                store_name, 
-                address, 
-                latitude, 
-                longitude, 
-                heart_count,
-				is_order_online,
+                s.store_id, 
+                s.store_name, 
+                s.address, 
+                s.latitude, 
+                s.longitude,
+                s.thumbnail_url,
+                s.heart_count,
+                s.is_order_online,
                 (6371 * ACOS(
-                    COS(RADIANS(?)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS(?)) + 
-                    SIN(RADIANS(?)) * SIN(RADIANS(latitude))
-                )) AS distance
-            FROM stores
+                    COS(RADIANS(?)) * COS(RADIANS(s.latitude)) * COS(RADIANS(s.longitude) - RADIANS(?)) + 
+                    SIN(RADIANS(?)) * SIN(RADIANS(s.latitude))
+                )) AS distance,
+                COUNT(r.store_review_id) AS review_count
+            FROM stores s
+            LEFT JOIN store_reviews r ON s.store_id = r.store_id
+            GROUP BY s.store_id
             HAVING distance <= ? 
-			${sortOrder}
+            ${sortOrder}
             LIMIT ?;
         `;
 
